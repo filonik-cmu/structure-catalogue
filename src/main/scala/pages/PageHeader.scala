acc5761c7ae4ee994c0ed62ea44870f69f168b94
filonik-cmu <132518907+filonik-cmu@users.noreply.github.com>
2023-05-23 23:19:20 -0400

    Fix logo

--- src/main/scala/pages/PageHeader.scala
+++ src/main/scala/pages/PageHeader.scala
@@ -2,18 +2,29 @@ package pages
 
 import com.raquo.laminar.api.L.*
 
-object PageHeader {
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
 
-  def apply(): HtmlElement =
+import org.scalajs.dom
+
+object PageHeader {
+  @js.native
+  @JSImport("/favicon.svg", JSImport.Default)
+  private object _logo extends js.Any
+  def logo: String = _logo.asInstanceOf[String]
+
+  def apply(): HtmlElement = {
+    dom.console.log(logo)
     headerTag(
       cls := "flex flex-row items-center justify-center",
       img(
         cls :="w-10 h-10 mx-2 dark:invert",
-        src := "/favicon.svg",
+        src := logo,
       ),
       h1(
         a(href := "/", "The Data Structure Catalogue")
       ),
     )
+  }
 
 }
